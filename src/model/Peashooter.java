d80abb4a97a0fb259099b9dfcca9d15bbc2a0f35
brian <brianwu312@gmail.com>
2023-06-03 18:31:35 -0700

    added new types of plants

--- src/model/Peashooter.java
+++ src/model/Peashooter.java
@@ -2,22 +2,33 @@ package model;
 
 import ui.GamePanel;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Peashooter extends Plant{
 
     private final static int SPAWN_TIME = 70;
     private RandSpawnManager projectileManager;
     private int health = 20;
+    private Image image;
     public Peashooter(int x, int y, GamePanel g){
         super(x,y, g);
-        this.projectileManager = new RandSpawnManager(70, g, RandSpawnManager.Type.BULLET);
+        this.projectileManager = new RandSpawnManager(SPAWN_TIME, g, RandSpawnManager.Type.BULLET);
+        try{
+            this.image = ImageIO.read(new File("src/Graphics/Peashooter.png"));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.fillRoundRect(x, y, 48, 48, 35, 35);
+        //g.setColor(Color.GREEN);
+        //g.fillRoundRect(x, y, 48, 48, 35, 35);
+        g.drawImage(image, x-20, y, null);
         g.setColor(Color.white);
         g.drawString(Integer.toString(health), x + 15, y + 24);
         projectileManager.drawEach(g);
