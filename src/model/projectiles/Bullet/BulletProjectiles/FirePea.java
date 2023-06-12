b755d9334309795dec9b0b4c5c7432359d4a6278
jorda <jyang74@students.ubc.ca>
2023-06-11 21:02:43 -0700

    Some sussy baka changes to make torchwood work ._. (part 2)

--- src/model/projectiles/Bullet/BulletProjectiles/FirePea.java
+++ src/model/projectiles/Bullet/BulletProjectiles/FirePea.java
@@ -10,7 +10,7 @@ public class FirePea extends Bullet {
     static final int width = 16;
     static final int height = 16;
     static final int speed = 5;
-    static final int damage = 1;
+    static final int damage = 2;
     static final int lifetime = 320;
     static final Color  color = new Color(0, 100, 0);
 
