500cbacf83335bd28570738b9c5f63c59d00b958
jorda <jyang74@students.ubc.ca>
2023-07-02 16:30:54 -0700

    immune to concurrent modification lesgooo

--- src/model/SpawnManager/ProjectileManager/ProjectileManagers/PultManager.java
+++ src/model/SpawnManager/ProjectileManager/ProjectileManagers/PultManager.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class PultManager extends ProjectileManager {
     public PultManager(GamePanel g) {
         super(g);
+
     }
 
     public boolean checkCollision(Projectile p) {
